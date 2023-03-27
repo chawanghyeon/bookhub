0a79cdbe66cbbd5d2ba58c8d508e608523e503dd
chawanghyeon <chawanghyeon@gmail.com>
2023-03-27 12:44:12 +0900

    Refactor tests.py in star

--- star/tests.py
+++ star/tests.py
@@ -24,9 +24,6 @@ class StarViewSetTestCase(APITestCase):
         self.user1_token = RefreshToken.for_user(self.user1)
 
     def test_create_star(self):
-        self.client.credentials(
-            HTTP_AUTHORIZATION=f"Bearer {self.user1_token.access_token}"
-        )
         response = self.client.post(
             reverse("star-list"), {"repository": self.repository.id}
         )
@@ -38,9 +35,6 @@ class StarViewSetTestCase(APITestCase):
         self.assertEqual(self.repository.star_count, 1)
 
     def test_destroy_star(self):
-        self.client.credentials(
-            HTTP_AUTHORIZATION=f"Bearer {self.user1_token.access_token}"
-        )
         Star.objects.create(user=self.user1, repository=self.repository)
         self.repository.star_count = 1
         self.repository.save()
@@ -54,26 +48,26 @@ class StarViewSetTestCase(APITestCase):
         self.assertEqual(self.repository.star_count, 0)
 
     def test_retrieve_stars(self):
-        self.client.credentials(
-            HTTP_AUTHORIZATION=f"Bearer {self.user1_token.access_token}"
-        )
         Star.objects.create(user=self.user1, repository=self.repository)
         response = self.client.get(reverse("star-detail", args=[self.user1.id]))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data["results"]), 1)
 
     def test_create_star_no_auth(self):
+        self.client.credentials()
         response = self.client.post(
             reverse("star-list"), {"repository": self.repository.id}
         )
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_destroy_star_no_auth(self):
+        self.client.credentials()
         star = Star.objects.create(user=self.user1, repository=self.repository)
         response = self.client.delete(reverse("star-detail", args=[star.id]))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_retrieve_stars_no_auth(self):
+        self.client.credentials()
         Star.objects.create(user=self.user1, repository=self.repository)
         response = self.client.get(reverse("star-detail", args=[self.user1.id]))
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
